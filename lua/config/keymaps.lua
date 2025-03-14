6bf2dff4a7274e57a7231550b9acada026245171
windx-foobar <bezalkogoln1ycoder@gmail.com>
2025-03-15 01:48:57 +0300

    chore: initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "package.json", -- Указываем конкретное имя файла
+  callback = function()
+    -- Устанавливаем keymap только для package.json
+    vim.api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "<leader>pp",
+      "<cmd>lua require('package-info').change_version()<cr>",
+      { noremap = true, silent = true, desc = "Install a different dependency version" }
+    )
+  end,
+})
---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,22 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      eslint = {
+        settings = {
+          rulesCustomizations = {
+            { rule = "curly", severity = "off" },
+          },
+        },
+      },
+
+      vtsls = {
+        settings = {
+          javascript = {
+            suggestionActions = false,
+          },
+        },
+      },
+    },
+  },
+}
