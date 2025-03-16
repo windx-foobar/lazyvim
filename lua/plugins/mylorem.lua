36f13d35e445992cb2d5f52e645af3074d7f93c8
windx-foobar <bezalkogoln1ycoder@gmail.com>
2025-03-17 00:49:17 +0300

    feat: add mylorem plugin

---
+++ lua/plugins/mylorem.lua
@@ -0,0 +1,10 @@
+return {
+  "elvxk/mylorem.nvim",
+  lazy = false,
+  opts = {
+    luasnip = true, -- Enable LuaSnip
+    ultisnips = false, -- Disable UltiSnips
+    vsnip = false, -- Disable VSnip
+    default = true, -- Use LuaSnip if others are not active
+  },
+}
