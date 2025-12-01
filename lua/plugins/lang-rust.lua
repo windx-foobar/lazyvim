d58696affc7f0d5ca5e8e0ac3e35c1939d9b3bba
windx-foobar <bezalkogoln1ycoder@gmail.com>
2025-12-02 02:07:49 +0300

    chore: patch latest changes lang-rust

--- lua/plugins/lang-rust.lua
+++ lua/plugins/lang-rust.lua
@@ -42,23 +42,54 @@ return {
     end,
   },
 
-  -- NOTE: Uncomment this is needed warns `async_trait`
-  -- {
-  --   "mrcjkb/rustaceanvim",
-  --   init = function(_)
-  --     vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, {
-  --       server = {
-  --         default_settings = {
-  --           ["rust-analyzer"] = {
-  --             procMacro = {
-  --               ignored = {
-  --                 ["async-trait"] = { "async_trait" },
-  --               },
-  --             },
-  --           },
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- },
+  {
+    "mrcjkb/rustaceanvim",
+    -- NOTE: Using latest changes from LazyVim/LazVim#main
+    init = function(_)
+      local exclude = {
+        ".direnv",
+        ".git",
+        ".jj",
+        ".github",
+        ".gitlab",
+        "bin",
+        "node_modules",
+        "target",
+        "venv",
+        ".venv",
+      }
+
+      vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, {
+        server = {
+          default_settings = {
+            ["rust-analyzer"] = {
+              files = {
+                exclude = exclude,
+                excludeDirs = exclude,
+                -- Avoid Roots Scanned hanging, see https://github.com/rust-lang/rust-analyzer/issues/12613#issuecomment-2096386344
+                watcher = "client",
+              },
+            },
+          },
+        },
+      })
+    end,
+    -- NOTE: Avoid warnings `async_trait`
+    opts = function(_, opts)
+      opts = opts or {}
+      local local_opts = opts
+      local path = { "server", "default_settings", "rust-analyzer", "procMacro", "ignored" }
+
+      for i = 1, #path - 1 do
+        local_opts[path[i]] = local_opts[path[i]] or {}
+        local_opts = local_opts[path[i]]
+      end
+      local_opts[path[#path]] = {
+        ["napi-derive"] = { "napi" },
+        ["async-recursion"] = { "async_recursion" },
+      }
+
+      return opts
+    end,
+  },
 }
